3f0694c16b39a2dd77c2dc29a6e57d66b1f12767
بيتلي لمسة بيت <bytlylmstbyt@gmail.com>
2026-09-15 02:32:26 +0300

    Fix SPA routing so direct page URLs do not return 404

--- src/pages/ProjectStages.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ProjectStagesSimulator from "@/components/home/ProjectStagesSimulator";
-
-export default function ProjectStages() {
-  return <ProjectStagesSimulator />;
-}
