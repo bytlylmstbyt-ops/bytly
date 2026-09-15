65a592632c8820063fa7f09ba90a595cd89748f6
بيتلي لمسة بيت <bytlylmstbyt@gmail.com>
2026-09-15 03:04:51 +0300

    Add Marketing Agent page

---
+++ src/pages/MarketingAgent.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import MarketingAgentPanel from '@/components/admin/MarketingAgentPanel';
+
+export default function MarketingAgent() {
+  return <main className="min-h-screen bg-slate-50 px-4 py-6 md:px-8"><MarketingAgentPanel /></main>;
+}
