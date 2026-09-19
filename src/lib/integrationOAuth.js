cbd44c51a78f49fdfce1537a2ceab0b268e99d25
بيتلي لمسة بيت <bytlylmstbyt@gmail.com>
2026-09-19 15:48:52 +0300

    Request Gmail settings scope for real connection checks

--- src/lib/integrationOAuth.js
+++ src/lib/integrationOAuth.js
@@ -20,6 +20,7 @@ const GOOGLE_SCOPES = [
   "profile",
   "https://www.googleapis.com/auth/gmail.send",
   "https://www.googleapis.com/auth/gmail.readonly",
+  "https://www.googleapis.com/auth/gmail.settings.basic",
   "https://www.googleapis.com/auth/calendar",
   "https://www.googleapis.com/auth/drive",
   "https://www.googleapis.com/auth/spreadsheets",
